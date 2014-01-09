d7a1feaa8c31d56278acd745ba84657af50b0922
Jim Menard <jim@jimmenard.com>
2014-01-09 08:28:33 -0500

    added Clojure version of CL function

--- notes.lisp
+++ notes.lisp
@@ -13,4 +13,17 @@
     (loop (multiple-value-bind (r k v) (i)
 		(unless r (return))
 		(format t "[~d] ~s~30t== ~5:d~%" (incf n) k v)))))
- 
+
+ ; By the way, here's a Clojure version.
+(comment
+(require ['clojure.string :as 'str])
+
+(let [lines (str/split-lines (slurp "/etc/passwd"))
+      line-fields (for [line lines]
+                    (str/split line #":"))
+      shells (map #(if (> (count %) 6) (nth % 6) nil) line-fields)
+      freqs (frequencies shells)]
+  (println)
+  (doseq [[k v] freqs]
+    (printf "%s: %d\n" k v)))
+)
