6829250e79a72f4e7f495200e575445def7e00b1
Jim Menard <jim@jimmenard.com>
2018-08-12 13:12:59 -0400

    fixed seq playing

--- portmidi/seq.lisp
+++ portmidi/seq.lisp
@@ -1,82 +1,94 @@
 ;; (in-package :cl-user)
 ;; (defpackage :seq
 ;;   (:use :portmidi)
-;;   (:export :run))
+;;   (:export :song-play))
 ;; (in-package :seq)
+
+;;; ================ events ================
 
 (defstruct note
   (pitch 64 :type integer)              ; negative pitch == rest; see make-rest
   (duration 1/4 :type rational)
-  (velocity 127 :type integer)
-  (off-velocity 127 :type integer))
+  (velocity 64 :type integer)
+  (off-velocity 64 :type integer))
 
 (defun make-rest (&rest args)
   (apply #'make-note (cons :pitch (cons -1 args))))
 
-(defstruct seq
+;;; ================ track ================
+
+(defstruct track
+  (name "Unnamed" :type string)
+  (channel 1 :type integer)
+  (notes '() :type list)
+  (playing-notes (make-hash-table :test #'equal) :type hash-table))
+
+(defun track-remember-note (track note)
+  (setf (gethash note (track-playing-notes track)) note))
+
+(defun track-forget-note (track note)
+  (remhash note (track-playing-notes track)))
+
+;;; ================ song ================
+
+(defstruct song
   (name "Unnamed" :type string)
   (beats-per-measure 4 :type integer)
   (beat-unit 4 :type rational)
   (tempo 120 :type integer)
-  (channel 1 :type integer)
-  (notes '() :type list)
-  (playing-notes (make-hash-table :test #'equal)) :type hash-table)
+  (tracks '() :type list))
 
-(defun beat-length-seconds (mseq)
-  (* (seq-beat-unit mseq) (/ 60.0 (seq-tempo mseq))))
+(defun beat-length-seconds (s)
+  (* (song-beat-unit s) (/ 60.0 (song-tempo s))))
+
+;;; ================ playing ================
 
 (defun report-if-error (err)
   (unless (zerop err)
     (format t "~A~%" (portmidi:get-error-text err))))
 
-(defun note-on (pm-output mseq note)
-  (let ((pitch (note-pitch note)))
-    (unless (minusp pitch)
-      (setf (gethash note (seq-playing-notes mseq)) note)
-      (report-if-error
-       (portmidi:midi-write-short
-        pm-output 0
-        (pm:message (+ #x90 (seq-channel mseq))
-                    pitch
-                    (note-velocity note)))))))
+(defun play-note-event (pm-output status channel pitch velocity)
+  (unless (minusp pitch)
+    (report-if-error
+     (portmidi:midi-write-short
+      pm-output 0
+      (pm:message (+ status channel) pitch velocity)))))
 
-(defun note-off (pm-output mseq note)
-  (let ((pitch (note-pitch note)))
-    (unless (minusp pitch)
-      (report-if-error
-       (portmidi:midi-write-short
-        pm-output 0
-        (pm:message (+ #x80 (seq-channel mseq))
-                    pitch
-                    (note-off-velocity note)))))
-    (remhash note (seq-playing-notes mseq))))
+(defun play-note-on (pm-output channel note)
+  (play-note-event pm-output #x90 channel
+                   (note-pitch note) (note-velocity note)))
 
-(defun seq-turn-off-playing-notes (pm-output mseq)
-  (maphash #'(lambda (note _)
-               (report-if-error
-                (portmidi:midi-write-short
-                 pm-output 0
-                 (pm:message (+ #x80 (seq-channel mseq))
-                             (note-pitch note)
-                             (note-velocity note)))))
-           (seq-playing-notes mseq)))
+(defun play-note-off (pm-output channel note)
+  (play-note-event pm-output #x80 channel
+                   (note-pitch note) (note-off-velocity note)))
 
-(defun run (pm-output mseq)
-  (let ((beat-secs (beat-length-seconds mseq)))
-    (loop for note in (seq-notes mseq)
+(defun song-play (pm-output s)
+  (let* ((beat-secs (beat-length-seconds s))
+         ;; for now, in this test code, seq must have one track
+         (track (first (song-tracks s)))
+         (channel (track-channel track)))
+    (loop for event in (track-notes track)
        do (progn
-            (note-on pm-output mseq note)
-            (sleep (* beat-secs (note-duration note)))
-            (note-off pm-output mseq note))))
-  ;; not necessary; an example
-  (seq-turn-off-playing-notes pm-output mseq))
+            (track-remember-note track event)
+            (play-note-on pm-output channel event)
+            (sleep (* beat-secs (note-duration event)))
+            (play-note-off pm-output channel event)
+            (track-forget-note track event)))
+    (maphash (lambda (event _)
+               (declare (ignore _))
+               (play-note-off pm-output channel event))
+             (track-playing-notes track))))
 
 ;; sample
-(defun seq-test ()
-  (let ((my-seq (make-seq))
-        (test-output (pm:device-open-output 2))) ; SimpleSynth virtual input
+(defun song-test ()
+  (let* ((my-track (make-track))
+         (my-seq (make-song :tracks (list my-track)))
+         (test-output (pm:device-open-output 2))) ; SimpleSynth virtual input
     (loop for pitch in (reverse '(40 42 44 45 47 49 51 52))
        do
-         (setf (seq-notes my-seq) (cons (make-note :pitch pitch) (seq-notes my-seq))))
-    (run test-output my-seq)
+         (setf (track-notes my-track)
+               (cons (make-note :pitch pitch) (track-notes my-track))))
+    (setf (track-notes my-track) (append (track-notes my-track)
+                                         (reverse (track-notes my-track))))
+    (song-play test-output my-seq)
     (report-if-error (pm:device-close test-output))))
